267b84e150897a4e9b50c9c8939309096af4cd49
gitlab-az1 <gitlab.az1@gmail.com>
2024-09-13 13:35:15 -0300

    fix: aes encryption error at looking for the key in environment variables

--- src/crypto/weak-aes.ts
+++ src/crypto/weak-aes.ts
@@ -5,10 +5,6 @@ import { type Decrypted } from 'cryptx-sdk/symmetric/core';
 export function aesEncrypt(data: any, pass: Uint8Array | ArrayBuffer | SharedArrayBuffer): Promise<Uint8Array>;
 export function aesEncrypt(data: any, pass: Uint8Array | ArrayBuffer | SharedArrayBuffer, encoding: BufferEncoding): Promise<string>;
 export async function aesEncrypt(data: any, pass: Uint8Array | ArrayBuffer | SharedArrayBuffer, encoding?: BufferEncoding): Promise<string | Uint8Array> {
-  if(!process.env.SYMMETRIC_ENC_KEY) {
-    throw new Error('[SYMMETRIC_ENC_KEY] Unset encryption key');
-  }
-  
   const k = new SymmetricKey(Buffer.isBuffer(pass) ? pass : Buffer.from(pass), {
     algorithm: 'aes-256-cbc',
     usages: ['encrypt', 'decrypt', 'sign', 'verify'],
@@ -22,10 +18,6 @@ export async function aesEncrypt(data: any, pass: Uint8Array | ArrayBuffer | Sha
 }
 
 export async function aesDecrypt<T = any>(data: string | Uint8Array, pass: Uint8Array | ArrayBuffer | SharedArrayBuffer): Promise<Decrypted<T>> {
-  if(!process.env.SYMMETRIC_ENC_KEY) {
-    throw new Error('[SYMMETRIC_ENC_KEY] Unset encryption key');
-  }
-  
   const k = new SymmetricKey(Buffer.isBuffer(pass) ? pass : Buffer.from(pass), {
     algorithm: 'aes-256-cbc',
     usages: ['encrypt', 'decrypt', 'sign', 'verify'],
